20d4cc431f796b38ad1c588fcff483ead2fd1d90
parkno07 <parkno07@naver.com>
2020-09-22 14:25:17 +0900

    create project

---
+++ src/test/java/kipark/odb/OdbApplicationTests.java
@@ -0,0 +1,13 @@
+package kipark.odb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
